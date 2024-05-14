6dbf25d536de4dd8a4d53471eb82e685f549f920
momo-s5s3 <momo_56n_colon@icloud.com>
2024-05-14 18:17:55 +0900

    CI対応

--- scripts/functions/velocity.py
+++ scripts/functions/velocity.py
@@ -1,20 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def target_valocity(h, c):
-    v= np.tanh(h-c)+np.tanh(c)
+    v = np.tanh(h - c) + np.tanh(c)
     return v
 
+
 def main():
-    
-    h = np.arange(0,10,0.1)
+    h = np.arange(0, 10, 0.1)
     for c in range(10):
-        v = target_valocity(h,c)
-        plt.plot(h,v)
+        v = target_valocity(h, c)
+        plt.plot(h, v)
         plt.xlabel('Distance [m]')
         plt.ylabel('Velocity [m/s]')
-        plt.xlim([0,10])
-        plt.ylim([0,2])
-        plt.savefig('../../outputs/target_velocity_'+str(c)+'.png')
+        plt.xlim([0, 10])
+        plt.ylim([0, 2])
+        plt.savefig('../../outputs/target_velocity_' + str(c) + '.png')
         plt.clf()
 
 
